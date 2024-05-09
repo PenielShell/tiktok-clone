c713a8d485bfd2999f6382eff7c2d952390c1d80
Ogunrinde Eunice Modupe <31671705+PenielShell@users.noreply.github.com>
2024-05-09 12:51:27 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore} from 'firebase/firestore/lite';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBLR7w80djQRvvFtbx7MkeztOImTxhri3I",
-  databaseURL: "https://afclip-8d2cb.firebaseapp.com",
-  authDomain: "afclip-8d2cb.firebaseapp.com",
-  projectId: "afclip-8d2cb",
-  storageBucket: "afclip-8d2cb.appspot.com",
-  messagingSenderId: "843239499874",
-  appId: "1:843239499874:web:582c588604f6a07ef4093b",
-  measurementId: "G-ENSC8VVGT3"
+  apiKey: "",
+  databaseURL: "",
+  authDomain: ",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 
